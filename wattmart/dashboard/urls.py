da2991ce300e83a8cdd0ccfd36c691cb6c93db93
jayalwayscodes <johnsonjardocs@gmail.com>
2025-02-17 23:19:52 +0100

    connected dashboard to base

--- wattmart/dashboard/urls.py
+++ wattmart/dashboard/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 app_name = 'dashboard'
 
-urlpatterns = [path('', views.index, name='index')]
+urlpatterns = [
+    path('', views.index, name='index')
+    ]
--- wattmart/dashboard/views.py
+++ wattmart/dashboard/views.py
@@ -5,6 +5,6 @@ from item.models import Item
 
 @login_required
 def index(request):
-    items = Item.objects.filter(created_by=request.username)
+    items = Item.objects.filter(created_by=request.user)
 
     return render(request, 'dashboard/index.html', {'items': items})
