c7849b737a767e8c73fea6ddbc6e7093efa3b552
jayalwayscodes <johnsonjardocs@gmail.com>
2025-02-18 22:28:46 +0100

    modified the browse feature

--- wattmart/item/views.py
+++ wattmart/item/views.py
@@ -1,8 +1,28 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import NewItemForm, EditItemForm
-from .models import Item
+from .models import Category, Item
+
+def browse(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id)
+    })
 
 # Details page
 
@@ -22,7 +42,7 @@ def new(request):
 
         if form.is_valid():
             item = form.save(commit=False)
-            item.created_by = request.username
+            item.created_by = request.user
             item.save()
 
             return redirect('item:detail', pk=item.id)
