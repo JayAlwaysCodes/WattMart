0a9c415f164e4e8a870ac80f3fdccdcf0423cbe1
jayalwayscodes <johnsonjardocs@gmail.com>
2025-02-23 21:33:55 +0100

    modified base.html

---
+++ wattmart/staticfiles/js/serviceworker.js
@@ -0,0 +1,23 @@
+var CACHE_NAME = 'my-django-pwa-cache-v1';
+var urlsToCache = [
+    '/',
+    '/static/css/style.css', // Add other static files here
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
